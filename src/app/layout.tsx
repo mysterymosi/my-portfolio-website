8b2f004138477c8f74a6a368774f25f266b57bc5
mysterymosi <okimmosimiracle@gmail.com>
2024-09-25 03:14:49 +0100

    (main) chore: updates on metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,12 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Spencer Sharp',
+    template: '%s - Mosi Odey',
     default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+      'Mosi Odey - Software engineer, and food enthusiast',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I’m Mosi, a software engineer based in Lagos, Nigeria. I’m frontend heavy but honestly, after over 4 years of working professionally, I can learn and use any technology if it’s required. I love fried plantain and eggs.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
